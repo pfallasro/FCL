8ff84447b80a21df05bacbfbc10343b2c25e7ce3
Pablo Fallas <pfallasro@gmail.com>
2016-06-18 15:46:58 -0700

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fclApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fclApp
+ */
+angular.module('fclApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fclApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fclApp
+ */
+angular.module('fclApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
