2816109a82fdae47cb953e24be4f961f1846e874
Pablo Fallas <pfallasro@gmail.com>
2017-01-08 15:57:25 -0800

    Agregando links faltantes y rounting

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,30 +39,22 @@ angular
 
   .config(function ($routeProvider) {
     $routeProvider
+
+      //HOME
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/contactanos', {
-        templateUrl: 'views/contactanos.html',
-        controller: 'ContactanosCtrl'
-      })
-      .when('/proyectos', {
-        templateUrl: 'views/proyectosSociales.html'
-        // controller: 'proyectos'
-      })
-      .when('/agendaQuincenal', {
-        templateUrl: 'views/agendaQuincenal.html'
-        // controller: 'agendaQuincenal'
+
+      //PRIMARY MENU
+      .when('/quienessomos', {
+        templateUrl: 'views/quienessomos.html'
+        // controller: 'quienessomos'
       })
       .when('/eneltiempo', {
         templateUrl: 'views/eneltiempo.html'
         // controller: 'eneltiempo'
-      })
-      .when('/quienessomos', {
-        templateUrl: 'views/quienessomos.html'
-        // controller: 'quienessomos'
       })
       .when('/programa', {
         templateUrl: 'views/programa.html'
@@ -72,6 +64,85 @@ angular
         templateUrl: 'views/imm.html'
         // controller: 'IMMCtrl'
       })
+      .when('/contactanos', {
+        templateUrl: 'views/contactanos.html',
+        controller: 'ContactanosCtrl'
+      })
+
+      //SECONDARY MENU
+      .when('/proyectos', {
+        templateUrl: 'views/proyectosSociales.html'
+        // controller: 'proyectos'
+      })
+
+      //CENTRO DE LIDERAZGO
+      .when('/cursos', {
+        templateUrl: 'views/cursos.html'
+        // controller: 'IMMCtrl'
+      })
+      .when('/concursos', {
+        templateUrl: 'views/concursos.html'
+        // controller: 'IMMCtrl'
+      })
+      .when('/agendaQuincenal', {
+        templateUrl: 'views/agendaQuincenal.html'
+        // controller: 'agendaQuincenal'
+      })
+      .when('/liderespositivos', {
+        templateUrl: 'views/lideresPositivos.html'
+        // controller: 'liderespositivos'
+      })
+
+      //PRENSA
+      .when('/ultimasnoticias', {
+        templateUrl: 'views/ultimasNoticias.html'
+        // controller: 'ultimasnoticias'
+      })
+      .when('/testimonios', {
+        templateUrl: 'views/testimonios.html'
+        // controller: 'testimonios'
+      })
+      .when('/informes', {
+        templateUrl: 'views/informes.html'
+        // controller: 'informes'
+      })
+
+      //DANOS UNA MANO
+      .when('/contribuciones', {
+        templateUrl: 'views/contribuciones.html'
+        // controller: 'contribuciones'
+      })
+      .when('/voluntariado', {
+        templateUrl: 'views/voluntariado.html'
+        // controller: 'voluntariado'
+      })
+      .when('/tcu', {
+        templateUrl: 'views/tcu.html'
+        // controller: 'tcu'
+      })
+
+      //SERVICIOS
+      .when('/bienesraices', {
+        templateUrl: 'views/bienesraices.html'
+        // controller: 'bienesraices'
+      })
+      .when('/taller', {
+        templateUrl: 'views/taller.html'
+        // controller: 'taller'
+      })
+      .when('/presenciacomunitaria', {
+        templateUrl: 'views/presenciacomunitaria.html'
+        // controller: 'presenciacomunitaria'
+      })
+      .when('/asesorias', {
+        templateUrl: 'views/asesorias.html'
+        // controller: 'asesorias'
+      })
+      .when('/entrenamiento', {
+        templateUrl: 'views/entrenamiento.html'
+        // controller: 'entrenamiento'
+      })
+
       .otherwise({
         redirectTo: '/'
       });
